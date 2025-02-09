fc802ecd372573eb3f0e5910bb6c2d27e02c23e1
Mariana Kolesnichenko <mariana@anitechsolutions.com>
2025-02-09 21:03:28 +0200

    Updated getter-setters

--- lesson5/src/getter-setters.js
+++ lesson5/src/getter-setters.js
@@ -1,6 +1,6 @@
 const building = {
     type: 'house',
-    room: 4,
+    _room: 4,
     buildingLength: 10,
     buildingWidth: 30,
     characteristics: {
@@ -8,23 +8,38 @@ const building = {
         price: 100
     },
 
-    get fullInfo() {
-        return `${this.type} type, ${this.room} rooms, ${this.buildingLength} length, ${this.buildingWidth} width, ${this.characteristics.typeOfHouse} typeOfHouse, ${this.characteristics.price} price`;
+    get room() {
+        return this._room;
     },
-    set updatePrice(newPrice) {
-        if (newPrice > 0 && newPrice != this.characteristics.price) {
+    set room(newRoomCount) {
+        if (newRoomCount > 0 && newRoomCount !== this._room) {
+            this._room = newRoomCount;
+        } else {
+            console.error('Incorrect or same room count');
+        }
+    },
+
+    get fullInfo() {
+        return `${this.type}, ${this._room} rooms, ${this.buildingLength} length, ${this.buildingWidth} width, ${this.characteristics.typeOfHouse}, ${this.characteristics.price}`;
+    },
+    updatePrice(newPrice) {
+        if (newPrice > 0 && newPrice !== this.characteristics.price) {
             this.characteristics.price = newPrice;
         } else {
             console.error('Incorrect or old price');
         }
     },
-    calculateBuildingSize() {
+    calculateRoomsSize() {
         return this.buildingLength * this.buildingWidth;
     }
 };
 
-console.log("Full information:", building.fullInfo);
-building.updatePrice = 200;
-console.log("Updated price is: ", building.characteristics.price);
-console.log("Building size information: ", building.calculateBuildingSize());
+console.log(building.fullInfo);
 
+building.updatePrice(200);
+console.log("Updated price: ", building.characteristics.price);
+console.log("Room size: ", building.calculateRoomsSize());
+
+console.log("Old room count: ", building.room);
+building.room = 5;
+console.log("New room count: ", building.room);
